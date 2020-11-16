d3ec9b6968f54c63bf99fdaa2e26fa7af1d4c9b6
FractionalRadix <nerostjust@gmail.com>
2020-11-16 21:35:49 +0100

    Upgraded to AndroidX

--- app/src/main/java/com/cormontia/android/dicomc_echo/MainActivity.java
+++ app/src/main/java/com/cormontia/android/dicomc_echo/MainActivity.java
@@ -1,17 +1,17 @@
 package com.cormontia.android.dicomc_echo;
 
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.io.InputStream;
